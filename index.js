c6dd8de48cebc148c024795fa54e6f53308b32d9
Gloria Restrepo <grestrepo682@gmail.com>
2022-03-07 16:10:45 -0500

    Formateo index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express()
 
+
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
